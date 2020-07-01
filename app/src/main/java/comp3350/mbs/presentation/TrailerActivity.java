ed051371ec218466201eb845c4d2a885fcc21167
NickD-BHSc <duannd@myumanitoba.ca>
2020-07-01 14:07:55 -0500

    Backup with int hash just in case
    Code with int hash for trailer video file call left in just in case. Dynamically calls video files for playing trailers

--- app/src/main/java/comp3350/mbs/presentation/TrailerActivity.java
+++ app/src/main/java/comp3350/mbs/presentation/TrailerActivity.java
@@ -4,6 +4,8 @@ import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
@@ -21,6 +23,7 @@ public class TrailerActivity extends AppCompatActivity {
     private VideoView trailerView;
     private MediaController trailerController;
     private String movieTitle;
+    private String hashedTitle;
     //TextView trailerTitle = findViewById(R.id.trailerMovieTitle);//delete this
 
     @Override
@@ -32,13 +35,10 @@ public class TrailerActivity extends AppCompatActivity {
             String videoPath = "android.resource://comp3350.mbs/";
 
             TextView trailerTitle = (TextView) findViewById(R.id.trailerMovieTitle);
-            //movieTitle = getIntent().getExtras().getString("comp3350.mbs.presentation.TITLE");
-            movieTitle = "tlk";
+            movieTitle = getIntent().getExtras().getString("comp3350.mbs.presentation.TITLE");
             //String rawPath = R.raw.{movieTitle};
-            int movie = getResources().getIdentifier("raw/movieTitle", null, getPackageName());
-            //String cookedPath = videoPath+number;
-            String test = Integer.toString(movie);
-            trailerTitle.setText(test);
+            int movie = getResources().getIdentifier("raw/"+movieTitle, null, getPackageName());
+            trailerTitle.setText(movieTitle);
         }
 
         playVideo = (Button) findViewById(R.id.trailerPlayBtn);
@@ -48,17 +48,55 @@ public class TrailerActivity extends AppCompatActivity {
     }
 
     public void videoPlay(View v){
-        String videoPath = "android.resource://comp3350.mbs/"+R.raw.tlk;
-        String cookedPath = videoPath;
-       // trailerTitle.setText(cookedPath);
+        //get pieces of the path
+        //int movieHash = hashMovieTitle(movieTitle);
+        String clean = cleanString(movieTitle);
+/*
+        Log.i("movie hash: ", rev);
+        String ph = "tlk";
+        String movieID = Integer.toString(movieHash); //the movie ID (end of path)
+        char beg = movieTitle.charAt(movieTitle.length()-1); //last letter of movie
 
+        Log.i("string of hash ", movieID);
+        //assemble entire path
+        String moviePath = beg+movieID;*/
+        //int movie = getResources().getIdentifier("raw/"+moviePath, null, getPackageName());
+        Log.i("string of hash ", clean);
+        int movie = getResources().getIdentifier("raw/"+clean, null, getPackageName());
+        Log.i("myTag", "movie integer: "+movie);
+        String videoPath = "android.resource://comp3350.mbs/";
+
+
+        Log.i("myTag", "path: "+videoPath);
+        String cookedPath = videoPath+movie;
+
+        Log.i("myTag", "final path: "+cookedPath);
 
         Uri uri = Uri.parse(cookedPath);
         trailerView.setVideoURI(uri);
         trailerView.setMediaController(trailerController);
         trailerController.setAnchorView(trailerView);
-
         trailerView.start();
+    }
+
+    /*
+    * hash the movie title to an integer to call the correct video file
+    * */
+    public int hashMovieTitle(String title){
+        int hash = title.charAt(0);
+        for (int i = 0; i < title.length(); i++) {
+            hash += (hash + title.charAt(i));
+        }
+        return hash;
+    }
+
+    /*
+    * Cleaned string to call the appropriate video file
+    * */
+    public String cleanString(String title){
+        String ns = title.replaceAll("\\s+","");
+        String lc = ns.toLowerCase();
+        return lc;
     }
 
 }
