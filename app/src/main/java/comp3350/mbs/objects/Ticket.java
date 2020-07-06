11ed0fa7aab96a82a46fb00c60fabf335f3105ca
amyamccaskill <65993822+amyamccaskill@users.noreply.github.com>
2020-07-05 21:38:13 -0500

    Create ticket class
    Created a ticket class to store information on different types of tickets, that can be used to calculate info shown on summary page.

---
+++ app/src/main/java/comp3350/mbs/objects/Ticket.java
@@ -0,0 +1,86 @@
+package comp3350.mbs.objects;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class Ticket implements Parcelable {
+    private double price;
+    private String type = "General";
+    private int quantity = 0;
+    DecimalFormat moneyFormat = new DecimalFormat("0.00");
+
+    /**
+     * Constructor of Ticket class.
+     *
+     * @param price      is the price of one of this type of ticket.
+     * @param type   is the type of the ticket.
+     */
+    public Ticket(double price, String type) {
+        this.price = price;
+        this.type = type;
+    }//end Constructor
+
+    /**
+     * Constructor of Ticket class.
+     *
+     * @param price      is the price of one of this type of ticket.
+     * @param type      is the type of the ticket.
+     * @param quantity  is the number of tickets.
+     */
+    public Ticket(double price, String type, int quantity) {
+        this.price = price;
+        this.type = type;
+        this.quantity = quantity;
+    }//end Constructor
+
+    //================================================================
+    //Parcelable methods
+    protected Ticket(Parcel in) {
+        price = in.readDouble();
+        type = in.readString();
+        quantity = in.readInt();
+    }
+
+    public static final Creator<Ticket> CREATOR = new Creator<Ticket>() {
+        @Override
+        public Ticket createFromParcel(Parcel in) {
+            return new Ticket(in);
+        }
+
+        @Override
+        public Ticket[] newArray(int size) { return new Ticket[size]; }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeDouble(price);
+        parcel.writeString(type);
+        parcel.writeInt(quantity);
+    }
+
+    //================================================================
+    //Getter methods
+    public double getPrice() {
+        return price;
+    }//end getPrice
+
+    public String getType() {
+        return type;
+    }//end getType
+
+    public int getQuantity() {
+        return quantity;
+    }//end getQuantity
+
+    public String getPriceAsString(){ return moneyFormat.format(price); }
+
+
+}//end Ticket
