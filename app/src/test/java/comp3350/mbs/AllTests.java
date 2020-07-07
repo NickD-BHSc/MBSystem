1d80c562c4c1c1a1d65b8ffa9ed2e5cee82d0d5d
kagememeo <57367450+kagememeo@users.noreply.github.com>
2020-07-07 15:13:18 -0500

    Seating Tests

--- app/src/test/java/comp3350/mbs/AllTests.java
+++ app/src/test/java/comp3350/mbs/AllTests.java
@@ -4,12 +4,14 @@ import junit.framework.TestSuite;
 
 import comp3350.mbs.business.CalculateTotalsTest;
 
+
 import comp3350.mbs.objects.MovieTest;
 import comp3350.mbs.objects.SeatingTest;
 import comp3350.mbs.objects.TheatreTest;
 import comp3350.mbs.objects.TicketTest;
 import comp3350.mbs.objects.ViewingTimeTest;
 
+import comp3350.mbs.objects.SeatingTest;
 import comp3350.mbs.objects.TicketTest;
 
 /**
@@ -33,6 +35,7 @@ public class AllTests {
     {
         suite.addTestSuite(TicketTest.class);
         suite.addTestSuite( SeatingTest.class);
+
         suite.addTestSuite(TheatreTest.class);
         suite.addTestSuite(MovieTest.class);
         suite.addTestSuite(ViewingTimeTest.class);
