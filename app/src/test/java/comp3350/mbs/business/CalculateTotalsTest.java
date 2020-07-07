27ce7a904ee4d2503575f35e7046d81aea4bd097
amyamccaskill <65993822+amyamccaskill@users.noreply.github.com>
2020-07-06 22:45:30 -0500

    Add Test Classes
    Added Test Classes for the main test class, and business and object tests.

---
+++ app/src/test/java/comp3350/mbs/business/CalculateTotalsTest.java
@@ -0,0 +1,19 @@
+package comp3350.mbs.business;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import comp3350.mbs.objects.Ticket;
+
+public class CalculateTotalsTest extends TestCase {
+
+    public CalculateTotalsTest(String arg0)
+    {
+        super(arg0);
+    }
+
+
+}
