ad9c5883d01fd811ffd15b0e9f6adb8dd4af3b01
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2020-07-17 18:30:42 -0500

    Removed the viewingTimeList parameter in the stub database.

--- app/src/main/java/comp3350/mbs/persistence/DataAccessStub.java
+++ app/src/main/java/comp3350/mbs/persistence/DataAccessStub.java
@@ -61,19 +61,19 @@ public class DataAccessStub implements DataAccess{
         String movieDescription;
 
         movieDescription= "Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.";
-        Movie m1 = new Movie("Avengers Endgame", R.drawable.avengers_endgame, movieDescription, viewingTimes);
+        Movie m1 = new Movie("Avengers Endgame", R.drawable.avengers_endgame, movieDescription);
 
         movieDescription = "In this lauded Pixar animated film, married superheroes Mr. Incredible (Craig T. Nelson) and Elastigirl (Holly Hunter) are forced to assume mundane lives as Bob and Helen Parr after all super-powered activities have been banned by the government. While Mr. Incredible loves his wife and kids, he longs to return to a life of adventure, and he gets a chance when summoned to an island to battle an out-of-control robot. Soon, Mr. Incredible is in trouble, and it's up to his family to save him.";
-        Movie m2 = new Movie("The Incredibles", R.drawable.incredibles, movieDescription, viewingTimes);
+        Movie m2 = new Movie("The Incredibles", R.drawable.incredibles, movieDescription);
 
         movieDescription = "This Disney animated feature follows the adventures of the young lion Simba (Jonathan Taylor Thomas), the heir of his father, Mufasa (James Earl Jones). Simba's wicked uncle, Scar (Jeremy Irons), plots to usurp Mufasa's throne by luring father and son into a stampede of wildebeests. But Simba escapes, and only Mufasa is killed. Simba returns as an adult (Matthew Broderick) to take back his homeland from Scar with the help of his friends Timon (Nathan Lane) and Pumbaa (Ernie Sabella).";
-        Movie m3 = new Movie("The Lion King", R.drawable.lion_king, movieDescription, viewingTimes);
+        Movie m3 = new Movie("The Lion King", R.drawable.lion_king, movieDescription);
 
         movieDescription = "The Imperial Forces -- under orders from cruel Darth Vader (David Prowse) -- hold Princess Leia (Carrie Fisher) hostage, in their efforts to quell the rebellion against the Galactic Empire. Luke Skywalker (Mark Hamill) and Han Solo (Harrison Ford), captain of the Millennium Falcon, work together with the companionable droid duo R2-D2 (Kenny Baker) and C-3PO (Anthony Daniels) to rescue the beautiful princess, help the Rebel Alliance, and restore freedom and justice to the Galaxy.";
-        Movie m4 = new Movie("Star Wars", R.drawable.starwars, movieDescription, viewingTimes);
+        Movie m4 = new Movie("Star Wars", R.drawable.starwars, movieDescription);
 
         movieDescription = "Just before the destruction of the planet Krypton, scientist Jor-El (Marlon Brando) sends his infant son Kal-El on a spaceship to Earth. Raised by kindly farmers Jonathan (Glenn Ford) and Martha Kent (Phyllis Thaxter), young Clark (Christopher Reeve) discovers the source of his superhuman powers and moves to Metropolis to fight evil. As Superman, he battles the villainous Lex Luthor (Gene Hackman), while, as novice reporter Clark Kent, he attempts to woo co-worker Lois Lane (Margot Kidder).";
-        Movie m5 = new Movie("Superman", R.drawable.superman, movieDescription, viewingTimes);
+        Movie m5 = new Movie("Superman", R.drawable.superman, movieDescription);
 
         //adding movies for movieListOne
         movieListOne.add(m1);
