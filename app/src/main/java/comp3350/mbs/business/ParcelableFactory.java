5b6aeea579728e448319b80a55bd93bf89fea2f7
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2020-07-25 18:50:46 -0500

    Created a Factory for Parcelable objects.

---
+++ app/src/main/java/comp3350/mbs/business/ParcelableFactory.java
@@ -0,0 +1,36 @@
+package comp3350.mbs.business;
+
+import android.os.Parcelable;
+
+import comp3350.mbs.objects.Seat;
+import comp3350.mbs.objects.Theatre;
+import comp3350.mbs.objects.TheatreMovies;
+import comp3350.mbs.objects.Ticket;
+import comp3350.mbs.objects.ViewingTime;
+import comp3350.mbs.presentation.ParcelableTheatre;
+import comp3350.mbs.presentation.ParcelableTheatreMovies;
+
+public class ParcelableFactory {
+
+    public static Parcelable createParcelableObject(Object object){
+        Parcelable parcObject = null;
+
+        if(object instanceof Theatre){
+            Theatre theatre = (Theatre)object;
+            parcObject = new ParcelableTheatre(theatre.getName(),theatre.getAddress(),theatre.getDistance());
+        }else if(object instanceof TheatreMovies){
+            TheatreMovies theatreMovie = (TheatreMovies)object;
+            parcObject = new ParcelableTheatreMovies(theatreMovie.getTheatreName(),theatreMovie.getMovieName(),theatreMovie.getMoviePoster(),theatreMovie.getMovieDescription());
+        }else if(object instanceof ViewingTime){
+            ViewingTime viewingTime = (ViewingTime)object;
+            
+        }else if(object instanceof Seat){
+
+        }else if(object instanceof Ticket){
+
+        }//end if-elses
+
+        return parcObject;
+
+    }//end createParcelableObject
+}
