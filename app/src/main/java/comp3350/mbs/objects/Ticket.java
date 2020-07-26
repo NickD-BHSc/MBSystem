2da9ff50e47280beef2f7df33c14fe30adb38292
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2020-07-25 21:05:27 -0500

    removed implementation of Parcelable in Ticket class.

--- app/src/main/java/comp3350/mbs/objects/Ticket.java
+++ app/src/main/java/comp3350/mbs/objects/Ticket.java
@@ -1,21 +1,22 @@
 package comp3350.mbs.objects;
 
-import android.os.Parcel;
-import android.os.Parcelable;
 
 import java.text.DecimalFormat;
 
-public class Ticket implements Parcelable {
+public class Ticket {
     private double price;
     private String type;
     private int quantity = 1;
     DecimalFormat moneyFormat = new DecimalFormat("0.00");
 
+    //needed an empty constructor when implementing Parcelable interface in the ParcelableTheatreMovies class.
+    public Ticket(){}
+
     /**
      * Constructor of Ticket class.
      *
-     * @param price      is the price of one of this type of ticket.
-     * @param type   is the type of the ticket.
+     * @param price is the price of one of this type of ticket.
+     * @param type is the type of the ticket.
      */
     public Ticket(double price, String type) {
         this.price = price;
@@ -25,46 +26,16 @@ public class Ticket implements Parcelable {
     /**
      * Constructor of Ticket class.
      *
-     * @param price      is the price of one of this type of ticket.
-     * @param type      is the type of the ticket.
-     * @param quantity  is the number of tickets.
+     * @param price is the price of one of this type of ticket.
+     * @param type is the type of the ticket.
+     * @param quantity is the number of tickets.
      */
     public Ticket(double price, String type, int quantity) {
         this.price = price;
         this.type = type;
         this.quantity = quantity;
     }//end Constructor
-
-    //================================================================
-    //Parcelable methods
-    protected Ticket(Parcel in) {
-        price = in.readDouble();
-        type = in.readString();
-        quantity = in.readInt();
-    }
-
-    public static final Creator<Ticket> CREATOR = new Creator<Ticket>() {
-        @Override
-        public Ticket createFromParcel(Parcel in) {
-            return new Ticket(in);
-        }
-
-        @Override
-        public Ticket[] newArray(int size) { return new Ticket[size]; }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeDouble(price);
-        parcel.writeString(type);
-        parcel.writeInt(quantity);
-    }
-
+    
     //================================================================
     //Getter methods
     public double getPrice() {
@@ -81,5 +52,18 @@ public class Ticket implements Parcelable {
 
     public String getPriceAsString(){ return moneyFormat.format(price); }
 
+    //================================================================
+    //Setter methods
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
 }//end Ticket
