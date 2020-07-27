9b6440ed7c5add51f287dd4893ea9aebf75a5f27
NickD-BHSc <duannd@myumanitoba.ca>
2020-07-26 21:28:20 -0500

    Create TicketStubActivity.java
    still need to add xml part

---
+++ app/src/main/java/comp3350/mbs/presentation/TicketStubActivity.java
@@ -0,0 +1,16 @@
+package comp3350.mbs.presentation;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import comp3350.mbs.R;
+
+public class TicketStubActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_stub);
+    }
+
+}
