c5e4692eab5c4c0487f684838ef7892ef9bb4313
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2020-08-05 11:57:31 -0500

    deleted unused imports in CustomAdapter class.

--- app/src/main/java/comp3350/mbs/presentation/CustomAdapter.java
+++ app/src/main/java/comp3350/mbs/presentation/CustomAdapter.java
@@ -1,27 +1,10 @@
 package comp3350.mbs.presentation;
-
-import android.content.Context;
-import android.content.Intent;
-import android.os.Parcelable;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.List;
 
-import comp3350.mbs.R;
-import comp3350.mbs.business.ParcelableFactory;
-import comp3350.mbs.objects.Order;
-import comp3350.mbs.objects.Seat;
-import comp3350.mbs.objects.Theatre;
-import comp3350.mbs.objects.TheatreMovies;
-import comp3350.mbs.objects.ViewingTime;
 
 public abstract class CustomAdapter extends RecyclerView.Adapter <CustomAdapter.CustomViewHolder> {
 
@@ -29,7 +12,7 @@ public abstract class CustomAdapter extends RecyclerView.Adapter <CustomAdapter.
 
     /**
      * CustomAdapter Constructor
-     * @param itemList is the data stored in a list which could be a list of theatres, TheatreMovies, ViewingTime etc.
+     * @param itemList is the data stored in a list which could be a list of theatres, TheatreMovies, ViewingTime, Seats, or Order.
      */
     public CustomAdapter(List itemList){
         this.itemList = itemList;
@@ -79,10 +62,11 @@ public abstract class CustomAdapter extends RecyclerView.Adapter <CustomAdapter.
      * Class responsible for assigning the widgets depending on the given context (activity).
      */
     public abstract class CustomViewHolder extends RecyclerView.ViewHolder {
-
-        //TODO put this class in the CustomAdapter
         public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
-        }
+        }//end constructor
+
     }//end CustomViewHolder class
+
+
 }//end CustomAdapter class
