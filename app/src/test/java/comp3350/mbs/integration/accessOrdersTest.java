c07632b4d6daa9c26a9ef2d882001eca58716d10
NickD-BHSc <duannd@myumanitoba.ca>
2020-08-08 15:23:11 -0500

    Implementation of each Access Class that calls the persistence layer
    So far only base case tests are implemented. Still need to add in invalid and error cases.
    ALSO: might need to redo structure of Integration folder to have a BusinessPersistenceClass overall

--- app/src/test/java/comp3350/mbs/integration/accessOrdersTest.java
+++ app/src/test/java/comp3350/mbs/integration/accessOrdersTest.java
@@ -9,10 +9,14 @@ import comp3350.mbs.application.Services;
 import comp3350.mbs.application.Main;
 import comp3350.mbs.business.AccessOrders;
 import comp3350.mbs.business.AccessSeats;
+import comp3350.mbs.business.AccessTheatreMovies;
 import comp3350.mbs.business.AccessTheatres;
+import comp3350.mbs.business.AccessTickets;
 import comp3350.mbs.business.AccessViewingTimes;
 import comp3350.mbs.objects.Order;
 import comp3350.mbs.objects.Theatre;
+import comp3350.mbs.objects.TheatreMovies;
+import comp3350.mbs.objects.Ticket;
 import comp3350.mbs.objects.ViewingTime;
 
 
@@ -108,7 +112,75 @@ public class accessOrdersTest extends TestCase {
     }
 
     public void testAccessTheatres(){
-        Theatre t = new Theatre("TestTheatre1", "Location1", "45km");
+        List<Theatre> theatreList;
+        AccessTheatres at;
+        Theatre actual;
 
+        Services.closeDataAccess();
+
+        System.out.println("Starting Integration test for AccessTheatres to persistence layer\n");
+
+        Services.createDataAccess(Main.dbName);
+
+        at = new AccessTheatres();
+        theatreList = at.getTheatreList();
+        Theatre expected = new Theatre("Scotiabank Theatre", "817 St.James", "15.7km");
+        actual = theatreList.get(0);
+
+        assertEquals(actual.getName(), expected.getName());
+        assertEquals(actual.getAddress(), expected.getAddress());
+        assertEquals(actual.getDistance(), expected.getDistance());
+
+        Services.closeDataAccess();
+
+        System.out.println("\nFinished Integration test of AccessViewingTimes to persistence layer\n");
     }
+
+    public void testAccessTheatreMovies(){
+        List<TheatreMovies> tml;
+        AccessTheatreMovies atm;
+        TheatreMovies actual;
+
+        Services.closeDataAccess();
+
+        System.out.println("Starting Integration test for AccessTheatreMovies to persistence layer\n");
+
+        Services.createDataAccess(Main.dbName);
+
+        atm = new AccessTheatreMovies();
+        tml = atm.getMoviesFromTheatre("Cinema City Northgate");
+        TheatreMovies expected = new TheatreMovies("Cinema City Northgate","Avengers Endgame",0,"Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.");
+        actual = tml.get(0);
+
+        assertEquals(actual.getMovieName(), expected.getMovieName());
+        assertEquals(actual.getMovieDescription(), expected.getMovieDescription());
+        assertEquals(actual.getTheatreName(), expected.getTheatreName());
+
+        Services.closeDataAccess();
+
+        System.out.println("\nFinished Integration test of AccessTheatreMovies to persistence layer\n");
+    }
+
+    public void testAccessTickets(){
+        AccessTickets at;
+        Ticket actual;
+
+        Services.closeDataAccess();
+
+        System.out.println("Starting Integration test for AccessTickets to persistence layer\n");
+
+        Services.createDataAccess(Main.dbName);
+
+        at = new AccessTickets();
+        Ticket expected = new Ticket(20.0, "The Lion King");
+        actual = at.getTicket("The Lion King");
+
+        assertEquals(actual.getMovieName(), expected.getMovieName());
+        assertEquals(actual.getPrice(), expected.getPrice());
+
+        Services.closeDataAccess();
+
+        System.out.println("\nFinished Integration test of AccessTickets to persistence layer\n");
+    }
+    
 }
