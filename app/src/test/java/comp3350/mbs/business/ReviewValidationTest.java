c870828353af727b4b7a18773ff4495b9f85e158
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2020-08-09 12:54:03 -0500

    Created a Test class for ReviewValidationTest

---
+++ app/src/test/java/comp3350/mbs/business/ReviewValidationTest.java
@@ -0,0 +1,16 @@
+package comp3350.mbs.business;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class ReviewValidationTest extends TestCase {
+
+    public ReviewValidationTest(String arg0)
+    {
+        super(arg0);
+    }
+    
+
+
+}//end ReviewValidationTest
