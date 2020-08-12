4bd2365b8dd630e2a00bcf6e340375a34543d491
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2020-08-12 15:16:27 -0500

    Added CreateReviewTest in the test suite in RunAcceptanceTests.java

--- app/src/androidTest/java/comp3350/mbs/RunAcceptanceTests.java
+++ app/src/androidTest/java/comp3350/mbs/RunAcceptanceTests.java
@@ -4,13 +4,14 @@ import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
 
 import comp3350.mbs.acceptance.CreateOrderTest;
+import comp3350.mbs.acceptance.CreateReviewTest;
 import comp3350.mbs.acceptance.ViewTicketTest;
 
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ViewTicketTest.class,CreateOrderTest.class})
+@Suite.SuiteClasses({CreateOrderTest.class,ViewTicketTest.class, CreateReviewTest.class})
 public class RunAcceptanceTests {
     public RunAcceptanceTests() {
-        System.out.println("Acceptance tests for ViewTicketTest, CreateOrderTest, CreateReviewTest.");
+        System.out.println("Acceptance tests for CreateOrderTest, ViewTicketTest, CreateReviewTest.");
     }
-}
+}//end RunAcceptanceTests
